d4eaa74b334c9b744100ba59239143001c5da647
CalebBertrand <caleb.a.bertrand@gmail.com>
2018-09-17 22:46:55 -0500

    New Repo

---
+++ build/js/scrollifysetup.js
@@ -0,0 +1,13 @@
+$(function() {
+		$.scrollify({
+			section : "div.section",
+			scrollSpeed: 1400,
+			easing: "easeOutExpo",
+			before:function() {
+				// $.scrollify.current().children().hide();
+			},
+			after:function() {
+				// $.scrollify.current().children().fadeIn();
+			},
+		});
+	});
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+var uglify = require('gulp-uglify');
+
+gulp.task('sass', function () {
+    return gulp.src('sass/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('build/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'build'
+    },
+  })
+});
+
+gulp.task('imgs', function(){
+  return gulp.src('build/imgs/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin()))
+  .pipe(gulp.dest('build/min/imgs'))
+});
+
+gulp.task('clean:min', function () {
+    return del.sync('build/min');
+});
+
+gulp.task('cache:clear', function (callback) {
+    return cache.clearAll(callback)
+});
+
+gulp.task('uglify', function () {
+    return gulp.src('build/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/min/js'))
+});
+
+gulp.task('default', ['browserSync', 'sass'], function () {
+    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch('build/*.html', browserSync.reload);
+    gulp.watch('build/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('compress', function (callback) {
+    runSequence('clean:min',
+        ['sass', 'imgs', 'uglify'],
+        callback
+    )
+});
