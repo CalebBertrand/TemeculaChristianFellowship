da5680f862d994f2658cb6d506a875afe62b2b03
CalebBertrand <caleb.a.bertrand@gmail.com>
2018-09-29 22:06:58 -0500

    Testing Something

--- build/js/index.js
+++ build/js/index.js
@@ -1,31 +1,31 @@
 $(document).ready(function () {
-    
+
     function ToggleMenu() {
         $("#menu").fadeToggle(600);
     }
-    
-    ToggleMenu();
 
-    $("#hamburger, #x, #menu a").click(function() {
-        ToggleMenu();
-    });
-
-    var mobile = (WURFL.form_factor != 'Desktop' && WURFL.form_factor != 'Tablet');
-
-    if (WURFL.form_factor == "Smartphone") {
-        $(".fullscreen").height(window.innerHeight * 1.15);
-    }else{
-        $(".fullscreen").height(window.innerHeight);
-    }
-
-    $("#menu").css("padding-top", $("header").height());
-    
     function EnableDesktopImages() {
         $('#section-1').css('background-image', "url('imgs/mountains.jpg')");
         $('#section-2').css('background-image', "url('imgs/bibleswag.jpg')");
         $('#section-3').css('background-image', "url('imgs/biblestudy.jpg')");
         $('#section-4').css('background-image', "url('imgs/woodwall.jpg')");
     }
+
+    ToggleMenu();
+
+    $("#hamburger, #menu").click(function () { //Toggle menu if hamburger or menu is clicked 
+        ToggleMenu();
+    });
+
+    var mobile = (WURFL.form_factor != 'Desktop' && WURFL.form_factor != 'Tablet');
+
+    // if (WURFL.form_factor == "Smartphone") { // If on smartphone, make sections 0.15 times taller. Won't apply if scrollify set height is true
+    //     $(".fullscreen").height(window.innerHeight * 1.15);
+    // } else {
+    //     $(".fullscreen").height(window.innerHeight);
+    // }
+
+    $("#menu").css("padding-top", $("header").height());
 
     if (WURFL.form_factor == 'Desktop') {
         $(".section").css("background-attachment", "fixed");
@@ -36,33 +36,33 @@ $(document).ready(function () {
         $('.hide-on-mobile').hide();
         $.scrollify.disable();
         $('#section-1').prepend('<img src="imgs/mobilewoodwall.jpg" class="enable-on-mobile background-image fullheight">');
-    }else{
+    } else {
         $('.enable-on-mobile').hide();
         $('.hide-on-mobile').show();
 
         EnableDesktopImages();
     }
 
-    $(window).resize(function() {
-
+    $(window).resize(function () {
 
         if (!mobile) {
-            $(".fullscreen").height(window.innerHeight);
+            // $(".fullscreen").height(window.innerHeight);
         }
-        
-        
-        $("#menu").css("padding-top", $("header").height() + 45);
 
-        if (mobile || window.innerWidth < 479) {
-            $('.enable-on-mobile').show();
-            $('.hide-on-mobile').hide();
-        } else {
-            $('.enable-on-mobile').hide();
-            $('.hide-on-mobile').show();
-            if (!mobile) {
-                EnableDesktopImages();
-                $.scrollify.enable();
-            }
-        }
+
+        // $("#menu").css("padding-top", $("header").height() + 45); //Make sure hamburger and logo are visible over menu items
+
+        // if (mobile || window.innerWidth < 479) {
+        //     $('.enable-on-mobile').show();
+        //     $('.hide-on-mobile').hide();
+        // } else {
+        //     $('.enable-on-mobile').hide();
+        //     $('.hide-on-mobile').show();
+        //     if (!mobile) {
+        //         EnableDesktopImages();
+        //         $.scrollify.enable();
+        //     }
+        // }
+
     });
 }); 
